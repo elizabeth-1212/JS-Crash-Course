44d844ee398c7b7afa0d587b79b411a5b2f698bb
Elizabeth <elizabethcommon12@gmail.com>
2025-06-19 21:38:08 -0400

    Java1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('abc123')
+let cash = 40
+let price = 40
+if (cash > price){
+    console.log(`you paid extra, here's ${cash-price} change`)
+}
+else if (cash === price){
+    console.log("you paid the exact amount, have a nice day!")
+}
+else {
+    console.log(`not enough money, you still owe ${price-cash}`)
+}
+
