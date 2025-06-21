adf6790cd3b6fa685e2d42aadde23a139279b123
Elizabeth <elizabethcommon12@gmail.com>
2025-06-20 21:47:36 -0400

    Java1

--- app.js
+++ app.js
@@ -1,21 +1,63 @@
-let cash = 20
-let price = 40
-let difference = cash - price
-if (cash > price){
-    console.log(`you paid extra, here's $${difference} change`)
-}
-else if (cash === price){
-    console.log("you paid the exact amount, have a nice day!")
-}
-else {
-    console.log(`not enough money, you still owe $${difference * -1}`)
-}
+//let cash = 20
+//let price = 40
+//let difference = cash - price
+//if (cash > price){
+//   console.log(`you paid extra, here's $${difference} change`)
+//}
+//else if (cash === price){
+//    console.log("you paid the exact amount, have a nice day!")
+//}
+//else {
+//    console.log(`not enough money, you still owe $${difference * -1}`)
+//}
 
 
 
-let money = 90
-let cost = 50
-let isStoreOpen = true
+//let money = 90
+//let cost = 50
+//let isStoreOpen = true
 
-let str = (money >= cost) && isStoreOpen ? 'give receipt' : "don't give receipt"
-console.log(str)
+//let str = (money >= cost) && isStoreOpen ? 'give receipt' : "don't give receipt"
+//console.log(str)
+
+
+
+//let count = 0
+//while (count <= 100) {
+//    console.log(count)
+//    count = count + 1
+//}
+
+
+//THE FOLLOWING WHILE AND FOR STATEMENTS PRODUCE THE SAME RESULT: FOR IS MORE COMMON
+
+//let count = 0
+//while (count <= 5) {
+//    console.log(count)
+//    count++
+//}
+
+
+//for(let i = 0; i < 5; i++ ){
+//    console.log(i)
+//}
+
+
+
+//for(let i = 1; i <= 20; ++i){
+  // if (i % 3 === 0 && i % 5 ===0){
+  //      console.log('Frontend Simplified')
+  //      }   
+  
+  //else if (i % 3 === 0){
+  //      console.log('Frontend')
+  //  }
+    
+  //  else if (i % 5 === 0){
+  //      console.log('Simplified')
+  //      }
+            
+  //  else{
+  //      console.log(i)
+  //  }
+//}
