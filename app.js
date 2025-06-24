15428c73e45b32d01e85474373b27ab8eba2b65d
Elizabeth <elizabethcommon12@gmail.com>
2025-06-23 21:35:30 -0400

    Java2

--- app.js
+++ app.js
@@ -119,6 +119,8 @@
 
 let dollars = [1, 5, 10, 3]
 let cents = []
-for (let i = 0; i < dollars.length; i++) 
-console.log(dollars[i] * 100);
+for (let i = 0; i < dollars.length; i++) {
+    cents.push(dollars[i] * 100)
+}
+console.log(cents);
     
