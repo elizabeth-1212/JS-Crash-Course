80855cc0e2b7a70f5152cef5ac11cb9c1e2d5a78
Elizabeth <elizabethcommon12@gmail.com>
2025-06-30 22:41:19 -0400

    Java2

--- app.js
+++ app.js
@@ -354,3 +354,15 @@ function progressiveSum (num){
 }
 console.log(progressiveSum(5));
 
+
+
+function calcTime(seconds){
+    let timerMinutes = Math.floor(seconds / 60);
+    let timerSeconds = seconds % 60;
+
+    if (timerMinutes.toString().length === 1){
+        timerMinutes = '0' + timerMinutes;
+    }
+    return timerMinutes + ':' + timerSeconds;
+}
+console.log(calcTime(500));
